8a3a85962878b100e101899a0473de7bb1094d30
Madhumitha Sakthi <msakthi@cs.utexas.edu>
2019-11-20 19:13:51 -0600

    changed

--- scripts/google_translate.py
+++ scripts/google_translate.py
@@ -1,14 +1,20 @@
 import csv
+import json
 from googletrans import Translator
 
-#load json data
 
 translated = []
-for line in json_file:
-    translator = Translator()
-    translator.translate(line['text'])
-    translated.append(translator.text)
+#load json data
+with open('/scratch/cluster/msakthi/Desktop/maddy/cs388/NLP-finalproject/NLP-Code-Switching/data/tweets_ur3sti_1.json') as json_file:
+    data = json.load(json_file)
+    for line in data:
+        translator = Translator()
+        print("line:", line)
+        print("line['text']", line['text'])
+        translator = translator.translate(line['text'])
+        print("translator.text", translator.text)
+        translated.append(translator.text)
 
-with open('output.csv','wb') as result_file:
-    wr = csv.writer(result_file, dialect='excel')
-    wr.writerow(translated)
+#with open('output.csv','wb') as result_file:
+#    wr = csv.writer(result_file, dialect='excel')
+#    wr.write(translated)
